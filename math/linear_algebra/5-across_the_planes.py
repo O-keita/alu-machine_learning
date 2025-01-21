afa9a74eb24b5427b39476c2908d71900adf7e2b
o-keita <o.keita@alustudent.com>
2025-01-21 16:27:01 +0000

    add metrices

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -12,7 +12,6 @@ def add_matrices2D(mat1, mat2):
 
     else:
         return [[mat1[i][j] + mat2[i][j] 
-             for j in range(len(mat1[0]))]
-             for i in range(len(mat1))
-             ]
+        for j in range(len(mat1[0]))]
+        for i in range(len(mat1))]
     
