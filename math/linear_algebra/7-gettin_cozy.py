ba22a00ce00919f746dad3b706d5beacebfd3ce6
o-keita <o.keita@alustudent.com>
2025-01-22 17:56:10 +0000

    things getting cozy

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -19,4 +19,5 @@ def cat_matrices2D(mat1, mat2, axis=0):
         if len(mat1) != len(mat2):
             return None
         return [row1[:] + row2[:] for row1, row2 in zip(mat1, mat2)]
+    return None
 
